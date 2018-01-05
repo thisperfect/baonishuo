991ff21df4a3904195d8538e8da662da2ca221d0
rundong <runxindong@gmail.com>
2018-01-05 14:03:16 +0800

    payment config

--- server/application/models/lib/WxPay.Config.php
+++ server/application/models/lib/WxPay.Config.php
@@ -22,10 +22,10 @@ class WxPayConfig
 	 * 获取地址：https://mp.weixin.qq.com/advanced/advanced?action=dev&t=advanced/dev&token=2005451881&lang=zh_CN
 	 * @var string
 	 */
-	const APPID = 'wx426b3015555a46be';
-	const MCHID = '1900009851';
-	const KEY = '8934e7d15453e97507ef794cf7b0519d';
-	const APPSECRET = '7813490da6f1265e4901ffb80afaa36f';
+	const APPID = 'wxfe95d603cb19e235';//小程序
+	const MCHID = '1494690562';
+	const KEY = 'WwhTZPsRuOsHHnVRfNJlFzJryiFuVYzS';
+	const APPSECRET = '53f70465740645a56feeee5db3901cf2';
 
 	//=======【证书路径设置】=====================================
 	/**
